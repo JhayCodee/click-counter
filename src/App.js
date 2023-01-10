3b0933c9914cf30eac696addd3d949a7b7c6c393
Luis Vargas <kluis0178@gmail.com>
2023-01-09 22:29:47 -0600

    app terminada

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import "./App.css";
+import { useState } from "react";
+import Button from "./Components/Button";
+import Counter from "./Components/Counter";
 import logo from "./img/freecodecamp-logo.png";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  const handleClick = () => {
+    setNumber(number + 1);
+  };
+
+  const restartCounter = () => {
+    setNumber(0);
+  };
+
   return (
     <div className="App">
       <div className="logo-container">
         <img className="app-logo" src={logo} alt="logo" />
       </div>
 
-      <div className="main-container"></div>
+      <div className="main-container">
+        <Counter num={number} />
+        <Button text="Click Me" isClickBtn={true} handleClick={handleClick} />
+        <Button
+          text="Restart"
+          isClickBtn={false}
+          handleClick={restartCounter}
+        />
+      </div>
     </div>
   );
 }
